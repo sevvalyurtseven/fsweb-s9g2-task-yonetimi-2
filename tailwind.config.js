3574b95ab6392425256f8b4b83b5795f913237ab
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2023-11-15 01:54:22 +0300

    Tailwind ile styling tamamlandi

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {
-      color: {
-        pill: "#ccc",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
