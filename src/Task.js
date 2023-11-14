81e208251b48ec9a1f1bebb8b82e9248c3625f18
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2023-11-15 00:11:54 +0300

    Date ayarlari yapildi.

--- src/Task.js
+++ src/Task.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { differenceInDays, formatDistance, formatDistanceToNow } from "date-fns";
+import {ta, tr} from "date-fns/locale";
 
 const Task = ({ taskObj, onComplete }) => {
+
+  const tarih = new Date(taskObj.deadline); //tarih objesi olusturduk. Bunu formatDistanceToNow'a gonderecegiz.
+  const kalanGun = formatDistanceToNow(tarih, {
+    addSuffix: true,
+    locale: tr
+  });
+
+  const accentClass = differenceInDays(tarih, new Date()) <= 3 ? "normal" : "urgent";
+
+  console.log(taskObj.deadline, differenceInDays(tarih, new Date()));
 
   return (
     <div className="task">
       <h3>{taskObj.title}</h3>
-      <div className="deadline">son teslim: <span>{taskObj.deadline}</span></div>
+      <div className="deadline">son teslim: <span className={accentClass} >{kalanGun}</span></div>
       <p>{taskObj.description}</p>
       <div>
         {taskObj.people.map((p) => (
